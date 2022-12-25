eb3cc1c976189786b355848712f988476c65a27e
terencebc92 <terencebc92@gmail.com>
2022-12-25 22:12:30 +0800

    Set up docker compose, finalize customer microservice

--- customer/src/main/java/com/amigoscode/customer/service/CustomerService.java
+++ customer/src/main/java/com/amigoscode/customer/service/CustomerService.java
@@ -2,10 +2,15 @@ package com.amigoscode.customer.service;
 
 import com.amigoscode.customer.dto.CustomerDto;
 import com.amigoscode.customer.model.Customer;
+import com.amigoscode.customer.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerService {
+
+    @Autowired
+    private CustomerRepository customerRepository;
 
     public void registerCustomer(CustomerDto customerDto) {
         Customer customer = Customer.builder()
@@ -14,6 +19,7 @@ public class CustomerService {
                 .email(customerDto.email())
                 .build();
 
+        customerRepository.save(customer);
 
     }
 
