61314fc63c1dbaa13153cca1a5f85b0ee35c90b3
terencebc92 <terencebc92@gmail.com>
2022-12-26 15:45:34 +0800

    Integrate Spring Cloud Sleuth and Zipkin

--- customer/src/main/java/com/amigoscode/customer/model/Customer.java
+++ customer/src/main/java/com/amigoscode/customer/model/Customer.java
@@ -1,10 +1,11 @@
 package com.amigoscode.customer.model;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 @Data
 @Builder
--- fraud/src/main/java/com/amigoscode/fraud/FraudApplication.java
+++ fraud/src/main/java/com/amigoscode/fraud/FraudApplication.java
@@ -2,6 +2,8 @@ package com.amigoscode.fraud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
 public class FraudApplication {
